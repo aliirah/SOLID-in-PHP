bf0618a72b7fd7b3dd81d49d80350a4033d06390
aliirah <alirahgoshy@gmail.com>
2022-03-18 07:33:45 +0330

    add comment for dependency inversion

--- src/DependencyInversion/SendWelcomeMessage.php
+++ src/DependencyInversion/SendWelcomeMessage.php
@@ -4,6 +4,9 @@ namespace App\DependencyInversion;
 
 class SendWelcomeMessage
 {
+
+//    Entities must depend on abstractions, not on concretions. It states that the high-level module must not depend on the low-level module, but they should depend on abstractions.
+
     private NotifiableInterface $notifiable;
 
     public function __construct(NotifiableInterface $notifiable)
